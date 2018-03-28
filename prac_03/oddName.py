a7462a3e4681fbd9a509afa182fe144f0bb4002f
AbelLim <abel.lim@my.jcu.edu.au>
2018-03-28 17:00:11 +0800

    Allow user to define the frequency of the letters printed.

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -2,16 +2,35 @@
 
 
 def main():
+    user_name = get_name()
+    frequency = get_frequency()
+    print_name(user_name, frequency)
+
+
+def print_name(user_name, frequency):
+    # Print every character in user_name
+    print("Showing every {} letter in your name.".format(frequency))
+    for char in user_name[::frequency]:
+        print(char)
+
+
+def get_name():
     # Ask user for name.
     user_name = str(input("Please enter your name: "))
-
     # Check if user_name is blank.
     while len(user_name.strip()) == 0:
         user_name = str(input("Please enter your name: "))
+    return user_name
 
-    # Print every character in user_name
-    print("Showing every odd letter in your name.")
-    for char in user_name[::2]:
-        print(char)
+
+def get_frequency():
+    # Ask user for frequency.
+    frequency = int(input("Please enter a number: "))
+
+    # Check if frequency is a non-zero integer.
+    while frequency == 0:
+        frequency = int(input("Please enter a number: "))
+    return frequency
+
 
 main()
