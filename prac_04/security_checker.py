3ad99634a71c5011604ef07565879bee267621db
AbelLim <abel.lim@my.jcu.edu.au>
2018-04-05 14:50:53 +0800

    Check username against a list of authorised username. Initial commit.

---
+++ prac_04/security_checker.py
@@ -0,0 +1,14 @@
+"""Check username against a list of authorised username"""
+
+
+def main():
+    print("Hello World")
+    get_username()
+
+
+def get_username():
+    username = str(input("Please enter your username: "))
+    return username
+
+
+main()
