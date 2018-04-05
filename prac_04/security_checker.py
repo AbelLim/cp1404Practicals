95de90a101d4c992207b7d2262622da3a5a9c103
AbelLim <abel.lim@my.jcu.edu.au>
2018-04-05 15:01:51 +0800

    Check username against authorised usernames.

--- prac_04/security_checker.py
+++ prac_04/security_checker.py
@@ -1,11 +1,22 @@
-"""Check username against a list of authorised username"""
+"""Check username against a list of authorised usernames."""
 
 
 def main():
-    print("Hello World")
-    get_username()
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+                 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+                 'InterpreterInterface', 'StartServer', 'bob']
+
+    username = get_username()
+
+    # Check if username matches the list of authorised usernames.
+    if username in usernames:
+        print("Access Granted")
+
+    else:
+        print("Access Denied")
 
 
+# Get username from user.
 def get_username():
     username = str(input("Please enter your username: "))
     return username
