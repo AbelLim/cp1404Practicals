7dfb1f9e8d9b10e6784eebf65fb1fdf1c2e79dcd
AbelLim <abel.lim@my.jcu.edu.au>
2018-04-12 13:56:51 +0800

    Add command "all" that prints all territories in dictionary.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -12,6 +12,9 @@ state = input("Enter short state: ")
 while state != "":
     if state.upper() in str(STATE_NAMES):
         print(state, "is", STATE_NAMES[state.upper()])
+    elif state.upper() == "ALL":
+        for key in STATE_NAMES:
+            print("{:<3} is {}".format(key, STATE_NAMES[key]))
     else:
         print("Invalid short state")
     state = input("Enter short state: ")
