fa60e113fb19398c4d83a5ba6e9b9585b731ab0f
AbelLim <abel.lim@my.jcu.edu.au>
2018-04-12 15:13:54 +0800

    Add word counting functionality to file.

--- prac_05/word_counter.py
+++ prac_05/word_counter.py
@@ -2,8 +2,20 @@
 
 
 def main():
+    # Get user input
     user_sentence = str(input("Please input a sentence: "))
-    word_occurrences = dict(zip(user_sentence.split(), [0]*len(user_sentence)))
+    # Split string for each white space
+    user_words = user_sentence.split()
+    # Convert string to dictionary. Each value set to zero.
+    word_occurrences = dict(zip(user_words, [0]*len(user_words)))
+
+    # Check for each word in dictionary. Add 1 if found.
+    for word in user_words:
+        if word in word_occurrences:
+            word_occurrences[word] = word_occurrences[word] + 1
+        else:
+            return
+
     print(word_occurrences)
 
 
