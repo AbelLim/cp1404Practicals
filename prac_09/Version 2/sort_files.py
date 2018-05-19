3b62ab41e6c69e5f988c9d64625f30d662c6ef9f
AbelLim <abel.lim@my.jcu.edu.au>
2018-05-20 05:07:40 +0800

    Sort files.

--- prac_09/Version 2/sort_files.py
+++ prac_09/Version 2/sort_files.py
@@ -9,17 +9,21 @@ def main():
     os.chdir('FilesToSort')
 
     # Go through all files
-    extensions = []
+    # Create relationships between files and subdirectories
+    relations = {}
     for directory_name, subdirectories, filenames in os.walk('.'):
         for filename in filenames:
             try:
-                if format(os.path.splitext(filename)[1]) not in extensions:
-                    directory = str(input('What category would you like to sort {} files into? '.format(os.path.splitext(filename)[1][1:])))
-                    os.mkdir(os.path.splitext(directory)[1])
-            except FileExistsError:
-                pass
-            finally:
-                os.rename(filename, '{}/{}'.format(os.path.splitext(filename)[1][1:], filename))
+                relations[os.path.splitext(filename)[1][1:]]
+            except KeyError:
+                directory = str(input(
+                    'What category would you like to sort {} files into? '.format(os.path.splitext(filename)[1][1:])))
+                try:
+                    os.mkdir(directory)
+                except FileExistsError:
+                    pass
+                relations[os.path.splitext(filename)[1][1:]] = directory
+            os.rename(filename, '{}/{}'.format(relations[os.path.splitext(filename)[1][1:]], filename))
 
 
 main()
