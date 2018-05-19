4a004452a91cfd314dac46a026298cdb6d35879a
AbelLim <abel.lim@my.jcu.edu.au>
2018-05-20 02:42:21 +0800

    Sort files into folders based on their file extension.

--- prac_09/Version 1/sort_files.py
+++ prac_09/Version 1/sort_files.py
@@ -7,7 +7,17 @@ import os
 
 def main():
     os.chdir('FilesToSort')
-    print("Starting directory is: {}".format(os.getcwd()))
+
+    # Go through all files
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for filename in filenames:
+            print(os.path.splitext(filename)[1])
+            try:
+                os.mkdir(os.path.splitext(filename)[1][1:])
+                print('Folder does not exist')
+            except FileExistsError:
+                print('Folder exists')
+            os.rename(filename, '{}/{}'.format(os.path.splitext(filename)[1][1:], filename))
 
 
 main()
