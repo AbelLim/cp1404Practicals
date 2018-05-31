0ebfa41176050ac8ef7f2ea5e2e33169ada15197
AbelLim <abel.lim@my.jcu.edu.au>
2018-05-31 18:03:40 +0800

    Initial commit

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,13 @@ from prac_07.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    strings = []
+    while True:
+        if len(strings) >= n:
+            break
+        strings.append(s)
+    new_string = " ".join(strings)
+    return new_string
 
 
 def is_long_word(word, length=5):
@@ -22,7 +28,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -32,7 +38,7 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
+    # 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -41,23 +47,26 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
-    test_car = Car(fuel=10)
+    test_car_2 = Car()
+    assert test_car_2.fuel == 0, "Car does not set default fuel correctly"
 
+    test_car_3 = Car(fuel=10)
+    assert test_car_3.fuel == 10, "Car does not set fuel correctly"
 
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
+# 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
 # doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+# 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
+# Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
@@ -66,3 +75,22 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+
+
+def make_sentence(phrase):
+    """
+    >>> make_sentence('hello')
+    'Hello.'
+    >>> make_sentence('It is an ex parrot.')
+    'It is an ex parrot.'
+    >>> make_sentence('This is a correct phrase.')
+    'This is a correct phrase.'
+    """
+    new_phrase = phrase[0].upper() + phrase[1:]
+    if new_phrase[-1] != '.':
+        new_phrase = new_phrase + '.'
+
+    return new_phrase
+
+
+doctest.testmod()
